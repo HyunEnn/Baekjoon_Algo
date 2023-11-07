17ae02799de64657416bc7ea4c60f2f1d5dba8b7
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2023-11-08 01:20:47 +0900

    [Silver III] Title: N과 M (5), Time: 416 ms, Memory: 42268 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr;
+	static int[] sel;
+	static boolean[] v;
+ 	static StringTokenizer st;
+ 	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		sel = new int[M];
+		v = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		recursive(0, 0);
+		System.out.println(sb.toString());
+	}
+	private static void recursive(int idx, int k) {
+		// basis
+		if(k == M) {
+			for(int i=0;i<sel.length;i++)
+				sb.append(sel[i] + " ");
+			sb.append("\n");
+			return;
+		}
+		
+		// inductive
+		for(int i=0;i<arr.length;i++) {
+			if(!v[i]) {
+				v[i] = true;
+				sel[k] = arr[i];				
+				recursive(i+1, k+1);
+				v[i] = false;
+			}
+		}
+		
+	}
+
+}
