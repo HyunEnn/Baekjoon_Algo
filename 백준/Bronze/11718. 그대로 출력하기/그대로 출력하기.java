145103de4d430301a176ed3b18b86a069a29705f
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2023-11-22 01:24:04 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String str = br.readLine();
+			if(str == null || str.isEmpty()) {
+				break;
+			}
+			sb.append(str).append("\n");
+		}
+		br.close();
+		System.out.println(sb.toString());
+	}
+
+}
