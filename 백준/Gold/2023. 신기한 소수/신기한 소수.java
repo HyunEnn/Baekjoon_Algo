469e72640096742658e8d0ec9cb1e79022aeb2d5
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-03-21 20:55:53 +0900

    [Gold V] Title: 신기한 소수, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,50 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static List<Integer> list;
+    static boolean[][] v;
+    static int[] firstPrime = {2, 3, 5, 7};
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        list = new ArrayList<>();
+        N = Integer.parseInt(br.readLine());
+        for(int i=0;i<4;i++) {
+            v = new boolean[10][9];
+            dfs(1, firstPrime[i]);
+        }
+//        for(int i : list) {
+//            System.out.println("list.get(i) = " + list.get(i));
+//        }
 
-	static int N;
-	static StringTokenizer st;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		sb = new StringBuilder();
-		dfs(2, 1);
-		dfs(3, 1);
-		dfs(5, 1);
-		dfs(7, 1);
-		
-		System.out.println(sb.toString());
-	}
-	private static void dfs(int idx, int k) {
-		// basis
-		if(k == N) {
-			if(isPrime(idx)) {
-				sb.append(idx + "\n");
-				return;				
-			}
-		}
-		
-		// inductive
-		for(int i=1;i<=9;i++) {
-			if(i%2 == 0) {
-				continue;
-			} // 들어오는 자리의 숫자가 소수인지 체크
-			
-			if(isPrime(idx*10+i)) {
-				dfs(idx * 10 +i, k+1);
-			} // 들어온 자리 숫자를 합한 전체 숫자가 소수인지 체크하고 dfs
-		}
-	}
-	
-	private static boolean isPrime(int num) {
-		for(int i=2;i<=num/2;i++) {
-			if(num % i == 0) 
-				return false;
-		}
-		return true;
-	}
+    }
 
+    public static void dfs(int idx, int prime) {
+        if(idx == N) {
+            if(isPrime(prime)) {
+                // 소수면 list 추가
+                list.add(prime);
+                System.out.println(prime);
+            }
+
+            return;
+        }
+
+        for(int i=1;i<=9;i++) {
+            if(i % 2 == 0)
+                continue;
+            if(isPrime(prime * 10 + i)) {
+                dfs(idx+1, prime * 10 + i);
+            }
+
+        }
+    }
+
+    public static boolean isPrime(int x) {
+        for(int i=2;i<=Math.sqrt(x);i++) {
+            if(x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
