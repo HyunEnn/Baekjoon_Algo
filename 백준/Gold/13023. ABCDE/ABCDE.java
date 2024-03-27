14518e2e5df48927d40de65c02230902bb1cd90a
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-03-27 22:42:00 +0900

    [Gold V] Title: ABCDE, Time: 264 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,86 +1,55 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[] v;
+    static ArrayList<Integer>[] list;
+    static int N, M;
+    static StringTokenizer st;
+    static boolean flag;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        flag = false;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        v = new boolean[N];
+        list = new ArrayList[N];
+        for(int i=0;i<N;i++) {
+            list[i] = new ArrayList<Integer>();
+        }
 
-//	static class Node {
-//		int v, e;
-//		public Node(int v, int e) {
-//			this.v = v;
-//			this.e = e;
-//		}
-//	}
-	static boolean[] visited;
-	static int N, M;
-	static int flag;
-	static int answer;
-	static List<Integer>[] list;
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 사람 수
-		M = Integer.parseInt(st.nextToken()); // 친구 관계 간선
-		list = new ArrayList[N];
-		visited = new boolean[N];
-		flag = 0;
-		answer = 0;
-		
-		for(int i=0;i<N;i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-		
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			list[v].add(e);
-			list[e].add(v);
-		}
-		
-//		BFS(0);
-		for(int i=0;i<N;i++) {
-			DFS(i, 1);
-			if(flag == 1)  {
-				answer=1;
-				break;
-			}
-		}
-		System.out.println(answer);
-	}
-	private static void DFS(int node, int depth) {
-		
-		if(depth == 5) {
-			flag = 1;
-			return;
-		}
+        for(int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
 
-		visited[node] = true;
-		
-		for(int i=0;i<list[node].size();i++) {
-			if(!visited[list[node].get(i)])
-				DFS(list[node].get(i), depth+1);
-		}
-		visited[node] = false;
-	}
-	
-//	private static void BFS(int node) {
-//		Queue<Integer> Q = new ArrayDeque<>();
-//		Q.offer(node);
-//		visited[node] = true; // 뽑은 값을 방문처리
-//		
-//		while(!Q.isEmpty()) {
-//			int newNode = Q.poll(); // Node 값을 저장
-//			for(int i=0;i<list[newNode].size();i++) {
-//				if(!visited[list[newNode].get(i)]) {
-//					Q.offer(list[newNode].get(i));
-//				}
-//			}
-//			
-//		}
-//	}
+        for(int i=0;i<N;i++) {
+            dfs(i, 1);
+            if(flag) break;
+        }
 
+        if(flag)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+
+    public static void dfs(int idx, int depth) {
+        if(depth == 5) {
+            flag = true;
+            return;
+        }
+
+        v[idx] = true;
+        for(int i=0;i<list[idx].size();i++) {
+            int next = list[idx].get(i);
+            if(!v[next])
+                dfs(next, depth+1);
+        }
+        v[idx] = false;
+    }
 }
