e321607d19891ff9a877a42b3c597353f095ff55
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-24 15:28:42 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15380 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,38 +1,38 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] sel;
+    static StringBuilder sb;
+    static boolean[] v;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sel = new int[M];
+        sb = new StringBuilder();
+        dfs(0, 1);
+        System.out.println(sb.toString());
+    }
 
-	static int n,m;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int[] sel = new int[m];
-		
-		recursive(1, 0, sel);
-		System.out.println(sb);
-	}
-	private static void recursive(int idx, int k, int[] sel) {
-		// basis
-		if(k == m) {
-			for(int x : sel) {
-				sb.append(x + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		// inductive
-		else {
-			for(int i=idx;i<=n;i++) {
-				sel[k] = i;
-				recursive(i, k+1, sel);
-			}
-		}
-	}
+    static void dfs(int idx, int curr) {
+        if(idx == M) {
+            for(int i=0;i<sel.length;i++) {
+                sb.append(sel[i]).append(" ");
+            } sb.append("\n");
+            return;
+        }
 
+        for(int i=curr;i<=N;i++) {
+            sel[idx] = i;
+            dfs(idx + 1, i);
+        }
+
+    }
 }
