add4a66f2ef42ebf98714b7e1a1b7e44a83a530d
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-24 15:07:52 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67912 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -2,38 +2,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static StringBuilder sb = new StringBuilder();
-    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb;
+    static int[] sel;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        recursive(0);
+        sel = new int[M];
+        sb = new StringBuilder();
+        dfs(0);
         System.out.println(sb.toString());
     }
 
-    public static void recursive(int cnt) {
-
-        if(cnt == M) {
-            for(int i : list) {
-                sb.append(i + " ");
-            }
-            sb.append("\n");
+    public static void dfs(int idx) {
+        if(idx == M) {
+            for(int i=0;i<sel.length;i++) {
+                sb.append(sel[i]).append(" ");
+            } sb.append("\n");
             return;
         }
 
-        for(int i=0;i<N;i++) {
-            list.add(i+1);
-            recursive(cnt + 1);
-            list.remove(list.size()-1);
+        for(int i=1;i<=N;i++) {
+            sel[idx] = i;
+            dfs(idx + 1);
         }
     }
 }
