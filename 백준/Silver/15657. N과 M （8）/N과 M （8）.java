27198e816b9ec8625e7894313156ed637194893c
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-25 13:25:26 +0900

    [Silver III] Title: N과 M (8), Time: 140 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,46 +1,45 @@
 
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] sel;
+    static int[] arr;
+    static boolean[] v;
+    static StringBuilder sb;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sel = new int[M];
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        sb = new StringBuilder();
+        dfs(0, 0);
+        System.out.println(sb.toString());
+    }
 
-	static int N, M;
-	static StringTokenizer st;
-	static int[] arr;
-	static int[] sel;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		sel = new int[M];
-		sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		recursive(0, 0);
-		System.out.println(sb.toString());
-		
-	}
-	
-	private static void recursive(int idx, int k) {
-		if(k == M) {
-			for(int i=0;i<sel.length;i++) {
-				sb.append(sel[i] + " ");
-			} sb.append("\n");
-			return;
-		}
-		
-		for(int i=idx;i<N;i++) {
-			sel[k] = arr[i];
-			recursive(i, k+1);
-		}
-	}
+    static void dfs(int idx, int curr) {
+        if(idx == M) {
+            for(int i=0;i<sel.length;i++) {
+                sb.append(sel[i]).append(" ");
+            } sb.append("\n");
+            return;
+        }
 
+        for(int i=curr;i<N;i++) {
+            sel[idx] = arr[i];
+            dfs(idx + 1, i);
+        }
+    }
 }
