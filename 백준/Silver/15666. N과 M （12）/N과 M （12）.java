15a33c24050dddf6c531b46c26851c9b391ad2c1
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-26 10:18:23 +0900

    [Silver II] Title: N과 M (12), Time: 136 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,55 +1,54 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] arr;
+    static Set<String> set = new HashSet<>();
+    static int[] sel;
+    static StringBuilder sb;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        sel = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        sb = new StringBuilder();
+        dfs(0, 0);
+        System.out.println(sb.toString());
+    }
 
-	static int[] arr;
-	static int N, M;
-	static List<Integer> list;
-	static int[] sel;
-	static StringTokenizer st;
+    static void dfs(int idx, int curr) {
+        if(idx == M) {
+            StringBuilder tempSb = new StringBuilder();
+            for(int i=0;i<sel.length;i++) {
+                tempSb.append(sel[i]).append(" ");
+            }
+            String result = tempSb.toString();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		list = new ArrayList<>();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[10000];
-		sel = new int[M];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			int a = Integer.parseInt(st.nextToken());
-			if (arr[a] == 0) {
-				arr[a]++;
-				list.add(a);
-			}
-		}
-		Collections.sort(list);
-//		System.out.println(list.size());
-//		for(int i=0;i<list.size();i++) {
-//			System.out.println(list.get(i));
-//		}
-		recursive(0, 0);
+            if(!set.contains(result)) {
+                set.add(result);
+                sb.append(result).append("\n");
+            }
+            return;
+        }
 
-	}
-
-	private static void recursive(int idx, int k) {
-		// basis
-		if (k == M) {
-			for (int i = 0; i < sel.length; i++)
-				System.out.print(sel[i] + " ");
-			System.out.println();
-			return;
-		}
-
-		// inductive
-		for (int i = idx; i < list.size(); i++) {
-			int a = list.get(i);
-			sel[k] = a;
-			recursive(i, k + 1);
-		}
-	}
-
+        for(int i=curr;i<N;i++) {
+            sel[idx] = arr[i];
+            dfs(idx + 1, i);
+        }
+    }
 }
