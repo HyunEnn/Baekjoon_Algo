a8b84da3f3036b82f255dbc9b7bc922c8139327f
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-11-14 15:34:12 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -29,25 +29,20 @@ public class Main {
         }
 
         int time = 0;      // 시간 초기화
-        int currentWeight = 0;  // 현재 다리 위의 트럭 총 무게
+        int weight = 0;  // 현재 다리 위의 트럭 총 무게
 
         while (!bridge.isEmpty()) {
-            time++; // 매 초마다 시간을 증가
+            time++;
+            int a = bridge.poll();
+            weight -= a;
 
-            // 다리에서 트럭을 한 칸씩 이동시킴 (다리 끝에서 트럭을 내림)
-            currentWeight -= bridge.poll();
-
-            // 트럭이 아직 남아있다면 다리에 진입을 시도
-            if (!trucks.isEmpty()) {
-                int nextTruck = trucks.peek();
-
-                // 다리에 트럭을 올릴 수 있는지 무게를 확인
-                if (currentWeight + nextTruck <= L) {
-                    bridge.offer(nextTruck);  // 트럭을 다리에 올림
-                    currentWeight += nextTruck;  // 현재 다리 무게 증가
-                    trucks.poll();  // 트럭을 대기 큐에서 제거
+            if(!trucks.isEmpty()) {
+                if(trucks.peek() + weight <= L) {
+                    int now = trucks.poll();
+                    weight += now;
+                    bridge.offer(now);
                 } else {
-                    bridge.offer(0);  // 무게가 초과하면 빈 칸을 추가해 다리를 채움
+                    bridge.offer(0);
                 }
             }
         }
