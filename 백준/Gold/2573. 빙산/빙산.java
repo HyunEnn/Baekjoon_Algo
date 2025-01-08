b3b60988b3ecbd99d55901b5bc0ea3bde554092c
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-01-08 12:58:47 +0900

    [Gold IV] Title: 빙산, Time: 556 ms, Memory: 116636 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,10 +1,9 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     static class Point {
         int r, c;
 
@@ -15,10 +14,11 @@ public class Main {
     }
 
     static int N, M;
-    static int[][] map;
     static boolean[][] v;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
+    static int[][] map;
+    static int[][] cMap;
     static List<Point> glaciers = new ArrayList<>();
     static List<Point> newGlaciers = new ArrayList<>();
     static StringTokenizer st;
@@ -29,109 +29,92 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        cMap = new int[N][M];
         v = new boolean[N][M];
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] > 0)
-                    glaciers.add(new Point(i, j)); // 빙산 추가
+                if (map[i][j] != 0) glaciers.add(new Point(i, j));
             }
         }
 
-        // 초기 값이 빙산이 분리되어 있는 지 체크
-        if (!checkDiv()) {
-            System.out.println("여기서 걸림?");
+        // 지도 복사
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cMap[i][j] = map[i][j];
+            }
+        }
+
+        if(dfs(glaciers.get(0).r, glaciers.get(0).c) != glaciers.size()) {
             System.out.println(0);
             System.exit(0);
         }
 
-        // 빙산을 녹이는 과정을 진행 , 종료는 빙산이 전부 다 녹았거나, 2개 이상으로 나눠지는 경우
-        // 2개 이상으로 나눠지는 게 아닌 빙산이 다 녹은 상태로 종료되면 분리되지 않은 것임
-        int idx = 1;
-        while(true) {
-            simulate();
-            if(glaciers.size() == 0) {
-                idx = 0;
-                break;
+        // 빙산이 있는 기준에서, 4개의 방향에 0의 개수마다 -1씩 진행하면서, 0이 되면 종료
+
+        // 지도를 갱신하지말고, 빙하를 갱신하자
+
+        // 빙산이 분리되는지 안되는지 체크해야함, 분리되면 종료
+
+        int year = 1;
+        while (true) {
+            // 모든 빙산 기준 탐색
+            v = new boolean[N][M];
+
+            for (Point p : glaciers) {
+                for (int k = 0; k < 4; k++) {
+                    int nr = p.r + dr[k];
+                    int nc = p.c + dc[k];
+                    if (inRange(nr, nc) && map[nr][nc] == 0) {
+                        cMap[p.r][p.c]--;
+                    }
+                }
+                if (cMap[p.r][p.c] <= 0) cMap[p.r][p.c] = 0; // -면 0으로 초기화
+                else newGlaciers.add(new Point(p.r, p.c)); // 계산 끝난 빙산들 추가
             }
-            if(!checkDiv()) {
-                break;
-            }
-            idx++;
-        }
 
-        System.out.println(idx);
-    }
-
-    public static boolean checkDiv() {
-        // glaciers 의 0번 인덱스를 통해 depth 탐색
-        Point p = glaciers.get(0);
-        v = new boolean[N][M];
-
-        // 탐색을 통해 나온 값이 glacier 와 같으면 빙하는 하나로 이루어져 있음.
-        return bfs(p);
-    }
-
-    public static boolean bfs(Point p) {
-        Queue<Point> Q = new ArrayDeque<>();
-        Q.offer(new Point(p.r, p.c));
-        v[p.r][p.c] = true;
-        int cnt = 1;
-
-        while(!Q.isEmpty()) {
-            Point cur = Q.poll();
-            for(int k=0;k<4;k++) {
-                int nr = cur.r + dr[k];
-                int nc = cur.c + dc[k];
-                if(inRange(nr, nc) && map[nr][nc] > 0 && !v[nr][nc]) {
-                    v[nr][nc] = true;
-                    cnt++;
-                    Q.offer(new Point(nr, nc));
+            // 기존 지도 갱신
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = cMap[i][j];
                 }
             }
+
+            glaciers.clear();
+            for (Point p : newGlaciers) {
+                glaciers.add(new Point(p.r, p.c));
+            }
+            newGlaciers.clear();
+
+            if(glaciers.isEmpty()) {
+                year = 0;
+                break;
+            }
+            int connect = dfs(glaciers.get(0).r, glaciers.get(0).c);
+            if(connect != glaciers.size()) break;
+            year++;
         }
-        return cnt == glaciers.size();
+
+        System.out.println(year);
+
     }
 
     public static boolean inRange(int r, int c) {
         return r >= 0 && r < N && c >= 0 && c < M;
     }
 
-    public static void simulate() {
-        // tmpMap 을 통해서 계산된 값을 다시 map 으로 이동
-        int[][] tmpMap = new int[N][M];
-        // glaciers 리스트들의 위치를 통해, 4방 탐색으로 주변에 0의 갯수만큼 -를 시킨다.
-        for (int i = 0; i < glaciers.size(); i++) {
-            Point p = glaciers.get(i);
-            int curr = map[p.r][p.c]; // 현재 빙하 값
-            int water = 0; // 주변 물의 갯수
-            for (int k = 0; k < 4; k++) {
-                int nr = p.r + dr[k];
-                int nc = p.c + dc[k];
-                if (inRange(nr, nc) && map[nr][nc] == 0)
-                    water++;
-            }
+    public static int dfs(int r, int c) {
+        v[r][c] = true;
+        int count = 1;
 
-            curr -= water;
-            if (curr > 0) {
-                tmpMap[p.r][p.c] = curr; // 0 이상일 경우에만 지도에 표시
-                // 그리고, 만약에 현재 위치의 빙하가 녹지 않았다면 newGlaciers 에 추가
-                newGlaciers.add(new Point(p.r, p.c));
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (inRange(nr, nc) && map[nr][nc] != 0 && !v[nr][nc]) {
+                count += dfs(nr, nc);
             }
         }
-
-        // 지도와 빙하들을 다시 갱신해줌
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                map[i][j] = tmpMap[i][j];
-            }
-        }
-        glaciers = new ArrayList<>();
-        for (Point p : newGlaciers) {
-            glaciers.add(new Point(p.r, p.c));
-        }
-        newGlaciers.clear();
+        return count;
     }
 }
