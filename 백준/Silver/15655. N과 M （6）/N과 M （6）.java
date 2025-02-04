f6cbb84e6f53bcbc757bdf7cba2c0b555c548de1
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-02-04 19:29:54 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,50 +1,46 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] sel;
-    static int[] arr;
-    static StringBuilder sb;
+
     static boolean[] v;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        sel = new int[M];
         arr = new int[N];
         v = new boolean[N];
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
-        sb = new StringBuilder();
-        dfs(0, 0);
+        recursive(0,0);
         System.out.println(sb.toString());
     }
 
-    static void dfs(int idx, int curr) {
+    public static void recursive(int curr, int idx) {
         if(idx == M) {
-            for(int i=0;i<sel.length;i++) {
-                sb.append(sel[i]).append(" ");
-            } sb.append("\n");
+            for(int i : list ) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
             return;
         }
 
         for(int i=curr;i<N;i++) {
-            if(!v[i]) {
-                v[i] = true;
-                sel[idx] = arr[i];
-                dfs(idx + 1, i);
-                v[i] = false;
-            }
+            list.add(arr[i]);
+            recursive(i+1, idx+1);
+            list.remove(list.size()-1);
         }
     }
+
 }
