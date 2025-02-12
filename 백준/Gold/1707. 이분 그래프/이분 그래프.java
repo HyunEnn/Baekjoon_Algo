9698ef24bbec266db94ffa9ad756c2494c720993
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-02-12 16:37:59 +0900

    [Gold IV] Title: 이분 그래프, Time: 864 ms, Memory: 270484 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,66 +1,67 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, V, E;
+    static List<Integer>[] graph;
+    static int[] color;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken()); // 정점
+            E = Integer.parseInt(st.nextToken()); // 간선
 
-	static int K, V, E;
-	static StringTokenizer st;
-	static ArrayList<Integer>[] list;
-	static boolean[] v;
-	static int[] ch;
-	static boolean isEven;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		K = Integer.parseInt(br.readLine());
-		for(int i=0;i<K;i++) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken()); // 정점 개수
-			E = Integer.parseInt(st.nextToken()); // 간선 개수
-			list = new ArrayList[V+1];
-			v = new boolean[V+1];
-			ch = new int[V+1];
-			isEven = true;
-			for(int j=0;j<=V;j++) {
-				list[j] = new ArrayList<Integer>();
-			}
-			
-			for(int j=0;j<E;j++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				list[a].add(b);
-				list[b].add(a);
-			}
-			
-			for(int j=1;j<=V;j++) {
-				if(isEven) 
-					DFS(j);
-				else 
-					break;
-			}
-			if(isEven) {
-				System.out.println("YES");
-			} else  {
-				System.out.println("NO");
-			}
-		}
-	}
+            graph = new ArrayList[V+1];
+            for(int j=1;j<=V;j++) {
+                graph[j] = new ArrayList<>();
+            }
+            color = new int[V+1];
+            for(int j=0;j<E;j++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                graph[s].add(e);
+                graph[e].add(s);
+            }
 
-	private static void DFS(int idx) {
-		v[idx] = true;
-		for(int i : list[idx]) {
-			if(!v[i]) {
-				ch[i] = (ch[idx] + 1) % 2;
-				DFS(i);
-			}
-			else if(ch[idx] == ch[i]) {
-				isEven = false;
-				break;
-			}
-		}
-		
-	}
+            boolean flag = false;
+            // 모든 정점에 대해서 dfs 탐색
+            for(int j=1;j<=V;j++) {
+                if(color[j] == 0) {
+                    color[j] = 1;
+                    if(!dfs(j)) { // 안되면 flag 처리
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if(!flag) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 
+    public static boolean dfs(int node) {
+
+        for(int i : graph[node]) {
+            // 방문하지 않은 정점
+            if(color[i] == 0) {
+                color[i] = color[node] * -1;
+                if(!dfs(i)) return false;
+            }
+            // 이미 방문한 곳인데, 현재의 color 와 같으면 조건 성립 X
+            else if(color[i] == color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
