e74687981de093775a1ca48b1849dbacf6026c5e
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-02-27 09:34:27 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -6,39 +6,32 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] arr;
-    static int[] dp;
+    static int[] arr, dp;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        dp = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        arr = new int[N];
-        for(int i=0;i<N;i++) {
+        for(int i=1;i<=N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
         }
 
-        init();
-        for(int i=1;i<N;i++) {
-            for(int j=0;j<i;j++) {
+        // 점화식 :
+        for(int i=2;i<=N;i++) {
+            for(int j=1;j<i;j++) {
                 if(arr[i] > arr[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
-        int max = 0;
-        for(int i=0;i<N;i++) {
-            max = Math.max(max, dp[i]);
+        int answer = 0;
+        for(int i=1;i<=N;i++) {
+            answer = Math.max(answer, dp[i]);
         }
-        System.out.println(max);
-    }
-
-    static void init() {
-        dp = new int[N];
-        for(int i=0;i<N;i++) {
-            dp[i] = 1;
-        }
+        System.out.println(answer);
     }
 }
