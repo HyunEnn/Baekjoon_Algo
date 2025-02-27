3691165c2a8c4ca4e33f9c132eb081593aef7c79
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-02-27 16:14:21 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,35 +1,31 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, K;
+    static int[] arr;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-	static int N, K;
-	static int[] money;
-	static int res;
-	static StringTokenizer st;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		money = new int[N];
-		res = 0;
-		
-		for(int i=0;i<N;i++) {
-			money[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(money); // 4200 % 1000 = 4
-//		System.out.println(Arrays.toString(money));
-		for(int i=N-1;i>=0;i--) {
-			if(money[i] > K) continue;
-			else {
-				res += K / money[i]; // 4
-				K = K % money[i]; // 200
-			}
-		}
-		System.out.println(res);
+        int ans = 0;
+        for(int i=N-1;i>=0;i--) {
+            if(arr[i] <= K) {
+                ans += K / arr[i];
+                K %= arr[i];
+            }
+        }
 
-	}
-
+        System.out.println(ans);
+    }
 }
