8628f4c3002e990a5cafdbf340417c8a6723248c
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-04-03 10:10:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,49 +1,43 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, S;
+    static int[] arr;
+    static int answer = 0;
+    static HashSet<String> set = new HashSet<>();
+    static boolean[] v;
+    static StringTokenizer st;
 
-	static int N, S;
-	static int[] arr;
-	static boolean[] v;
-	static int cnt;
-	static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        v = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		v = new boolean[N];
-		cnt = 0;
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		dfs(0, 0);
-		if (S == 0)
-			System.out.println(cnt - 1);
-        else
-		    System.out.println(cnt);
+        dfs(0, 0, 0);
 
-	}
+        System.out.println(answer);
+    }
 
-	private static void dfs(int idx, int num) {
-		// basis -> 끝까지 돌앗으면 return
-		if (idx == N) {
-			if (num == S)
-				cnt++;
-			return;
-		}
+    public static void dfs(int idx, int sum, int cnt) {
+        if(idx == N) {
+            if(sum == S && cnt > 0)
+                answer++;
+            return;
+        }
 
-		if (!v[idx]) {
-			v[idx] = true;
-			dfs(idx + 1, num + arr[idx]);
-			v[idx] = false;
-		}
-		dfs(idx+1, num);
-	}
-
+        dfs(idx + 1, sum + arr[idx], cnt + 1);
+        dfs(idx + 1, sum, cnt);
+    }
 }
