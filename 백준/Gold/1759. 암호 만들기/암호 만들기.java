e103010d78a6c9d48497b77ef5c1945671c654e2
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-04-03 14:17:39 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 15364 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,65 +1,58 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int L, C;
+    static char[] ch = new char[] {'a','e','i','o','u'};
+    static boolean[] v;
+    static char[] arr;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new char[C];
+        v = new boolean[C];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<C;i++) {
+            arr[i] = st.nextToken().charAt(0);
+        }
 
-	static int L, C;
-	static StringTokenizer st;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	static char[] alpha;
-	static char[] answer;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		L = Integer.parseInt(st.nextToken()); // 자리 수
-		C = Integer.parseInt(st.nextToken()); // 입력받는 char
-		visited = new boolean[C]; // 방문 배열
-		alpha = new char[C];
-		answer= new char[L];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<C;i++) {
-			alpha[i] = st.nextToken().charAt(0);
-		}
-		
-		Arrays.sort(alpha); // 사전식으로 정렬
-		
-		recursive(0, 0);
-		// acis iacs 
-	}
-	private static void recursive(int idx, int a) {
-		// basis
-		if(idx == L) {
-			if(isPossible()) {
-				System.out.println(answer);
-			}
-			return;
-		}
-		
-		// inductive
-		for(int i=a;i<C;i++) {
-			answer[idx] = alpha[i];
-			recursive(idx+1, i+1);
-		}
-	}
-	private static boolean isPossible() {
-		int v = 0; // 모음
-		int c = 0; // 자음
-		for(int i=0;i<L;i++) {
-			if(answer[i] == 'a' ||
-					answer[i] == 'e' ||
-					answer[i] == 'i' ||
-					answer[i] == 'o' ||
-					answer[i] == 'u') {
-				v++;
-			}
-			else
-				c++;
-		}
-		if(v >= 1 && c >= 2) 
-			return true;
-		return false;
-	}
+        Arrays.sort(arr);
 
+        dfs(0, new StringBuilder());
+    }
+
+    public static void dfs(int idx, StringBuilder sb) {
+        // basis
+        if(sb.length() == L) {
+            int a = 0;
+            int b = 0;
+            for(int i=0;i<sb.length();i++) {
+                boolean flag = false;
+                for(int j=0;j<ch.length;j++) {
+                    if(sb.charAt(i) == ch[j]) flag = true;
+                }
+                if(flag) a++;
+                else b++;
+            }
+            if(a>=1 && b>=2) System.out.println(sb.toString());
+            return;
+        }
+
+        // inductive
+        for(int i=idx;i<C;i++) {
+            if(!v[i]) {
+                v[i] = true;
+                sb.append(arr[i]);
+                dfs(i+1, sb);
+                sb.delete(sb.length()-1, sb.length());
+                v[i] = false;
+            }
+        }
+    }
 }
