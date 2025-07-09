531ce2f920d2b5b7ee11fbeafadcbf775c5adc6e
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-07-09 20:43:44 +0900

    [Gold IV] Title: 불, Time: 1180 ms, Memory: 105956 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,102 +1,95 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Point {
-        int r, c, cnt;
-        char rec;
-        Point(int r, int c, char rec, int cnt) {
-            this.r = r;
-            this.c = c;
-            this.rec = rec;
-            this.cnt = cnt;
-        }
-    }
-    static int W, H, T;
+    static int w, h;
     static char[][] map;
+    static int[][] fireTime;
+    static int[][] sangkeunTime;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
-    static StringTokenizer st;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-        for(int t=0;t<T;t++) {
-            st = new StringTokenizer(br.readLine());
-            W = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
-            map = new char[H][W];
-            Queue<Point> fire = new ArrayDeque<>();
-            Queue<Point> move = new ArrayDeque<>();
-            for(int i=0;i<H;i++) {
-                String line = br.readLine();
-                for(int j=0;j<W;j++) {
-                    map[i][j] = line.charAt(j);
-                    if(map[i][j] == '*') fire.offer(new Point(i, j, '*', 0));
-                    else if(map[i][j] == '@') move.offer(new Point(i, j, '@', 0));
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int tc = 0; tc < t; tc++){
+            w = sc.nextInt();
+            h = sc.nextInt();
+            map = new char[h][w];
+            fireTime = new int[h][w];  // 불이 도착하는 시간 저장
+            sangkeunTime = new int[h][w];  // 상근이가 도착하는 시간 저장
+
+            Queue<int[]> fQ = new ArrayDeque<>();
+            Queue<int[]> sQ = new ArrayDeque<>();
+
+            for (int i = 0; i < h; i++) {
+                String row = sc.next();
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = row.charAt(j);
+                    fireTime[i][j] = -1;  // 불 방문 안함(-1)으로 초기화
+                    sangkeunTime[i][j] = -1;  // 상근 방문 안함(-1)으로 초기화
+
+                    // 불 시작점 저장 및 시간 배열 0초로 갱신
+                    if (map[i][j] == '*') {
+                        fQ.add(new int[]{i, j});
+                        fireTime[i][j] = 0;
+                    }
+                    // 상근이 시작점 저장 및 시간 배열 0초로 갱신
+                    else if (map[i][j] == '@') {
+                        sQ.add(new int[]{i, j});
+                        sangkeunTime[i][j] = 0;
+                    }
                 }
             }
 
-            boolean flag = false;
-            int ans = 0;
-            List<Point> fireList = new ArrayList<>();
-            List<Point> moveList = new ArrayList<>();
-            Point p = fire.peek();
-            while(!move.isEmpty()) {
-                while(!fire.isEmpty()) {
-                    Point f = fire.poll();
-                    for(int k=0;k<4;k++) {
-                        int fr = f.r + dr[k];
-                        int fc = f.c + dc[k];
-                        // 범위 안에 있고 벽, 불이 없던곳으로 진행
-                        if(inRange(fr, fc) && map[fr][fc] != '#' && map[fr][fc] != '*') {
-                            map[fr][fc] = '*';
-                            fireList.add(new Point(fr, fc, '*', f.cnt + 1));
-                        }
+            // bfs로 각 칸에 대해 불이 퍼지는 시간 기록
+            while (!fQ.isEmpty()) {
+                int[] cur = fQ.poll();
+                int r = cur[0], c = cur[1];
+                for (int d = 0; d < 4; d++) {
+                    int nr = cur[0] + dr[d];
+                    int nc = cur[1] + dc[d];
+
+                    // map 범위 내, 벽이 아니고, 불이 방문한적 없으면 방문체크
+                    if(nr < h && nr >= 0 && nc < w && nc >= 0 && map[nr][nc] != '#' && fireTime[nr][nc] == -1){
+                        fireTime[nr][nc] = fireTime[r][c] + 1;
+                        fQ.add(new int[]{nr, nc});
                     }
                 }
-                for(Point f : fireList) {
-                    fire.offer(new Point(f.r, f.c, f.rec, f.cnt));
-                }
-                fireList = new ArrayList<>(); // 값을 복사하고, 초기화
-
-                while(!move.isEmpty()) {
-                    Point m = move.poll();
-                    for(int k=0;k<4;k++) {
-                        int mr = m.r + dr[k];
-                        int mc = m.c + dc[k];
-                        // 범위 안에 있고 빈칸이면 이동
-                        if(inRange(mr, mc) && map[mr][mc] == '.') {
-                            map[mr][mc] = '@';
-                            moveList.add(new Point(mr, mc, '@', m.cnt + 1));
-                        }
-
-                        // 범위를 벗어난다면, 탈출한것으로 간주
-                        else if(!inRange(mr, mc)) {
-                            flag = true;
-                            ans = m.cnt + 1;
-                            break;
-                        }
-                    }
-
-                    if(flag) break;
-                }
-
-                if(flag) break;
-                for(Point m : moveList) {
-                    move.offer(new Point(m.r, m.c, m.rec, m.cnt));
-                }
-                moveList = new ArrayList<>();
             }
 
-            System.out.println(ans == 0 ? "IMPOSSIBLE" : ans);
-            // @, *만 지속적으로 이동하는 방식으로 진행, 맵을 채우면?
+            // bfs로 상근이 이동 (불보다 빨리 도착할 수 있는 곳만 이동)
+            boolean check = false;  // 상근이 탈출 여부
+            int answer = -1;
+
+            while (!sQ.isEmpty()) {
+                int[] cur = sQ.poll();
+                int r = cur[0], c = cur[1];
+                for (int d = 0; d < 4; d++) {
+                    int nr = cur[0] + dr[d];
+                    int nc = cur[1] + dc[d];
+
+                    // map의 범위를 벗어나면 탈출
+                    if (nr < 0 || nr >= h || nc < 0 || nc >= w) {
+                        answer = sangkeunTime[r][c] + 1; // 이전 시간+1
+                        check = true;
+                        break;
+                    }
+
+                    // 벽x, 상근 방문x, (불 방문x || 불보다 먼저 도착)
+                    if (map[nr][nc] != '#' && sangkeunTime[nr][nc] == -1 &&
+                            (fireTime[nr][nc] == -1 || sangkeunTime[r][c]+1 < fireTime[nr][nc])){
+                        sangkeunTime[nr][nc] = sangkeunTime[r][c] + 1; // 상근이 도착 시간 갱신
+                        sQ.add(new int[]{nr, nc});
+                    }
+                }
+                // 탈출하면 break
+                if (check) break;
+            }
+
+            if (check) System.out.println(answer);
+            else System.out.println("IMPOSSIBLE");
         }
-    }
-
-    public static boolean inRange(int r, int c) {
-        return r >= 0 && r < H && c >= 0 && c < W;
     }
 }
