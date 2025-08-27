05521b38287ff9d5eb79c2f37c3242ea0a8570ff
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-08-27 14:08:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53732 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,33 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
+    static int[] weights;
+    static int[] values;
+    static int[][] dp;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        int[] weight = new int[N+1];
-        int[] value = new int[N+1];
+        weights = new int[N+1];
+        values = new int[N+1];
+        dp = new int[N+1][K+1];
 
         for(int i=1;i<=N;i++) {
             st = new StringTokenizer(br.readLine());
-            weight[i] = Integer.parseInt(st.nextToken());
-            value[i] = Integer.parseInt(st.nextToken());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[N+1][K+1];
         for(int i=1;i<=N;i++) {
-            // 물건들 개수만큼 돈다
             for(int j=1;j<=K;j++) {
-                // 선택된 물건이 가방의 크기보다 크다면 기존의 값 사용
-                if(j < weight[i])
-                    dp[i][j] = dp[i-1][j];
-                else
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - weight[i]] + value[i]);
+                if(weights[i] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weights[i]] + values[i]);
             }
         }
 
         System.out.println(dp[N][K]);
+      
     }
 }
