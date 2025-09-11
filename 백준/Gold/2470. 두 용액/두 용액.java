8236f6af2a716f28c07964c0c746e64b4c3405a6
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-09-11 13:56:13 +0900

    [Gold V] Title: 두 용액, Time: 348 ms, Memory: 28856 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,42 +1,41 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
     static int N;
     static int[] arr;
-    static int ansLeft, ansRight;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
         arr = new int[N];
+        st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-        twoPointer();
-        System.out.println(ansLeft + " " + ansRight);
-    }
 
-    public static void twoPointer() {
-        int left = 0, right = N - 1;
-        ansLeft = 0; ansRight = 0;
-        int ans = Integer.MAX_VALUE;
+        int left = 0, right = N-1, ans = Integer.MAX_VALUE;
+        int al = 0, san = 0;
         while(left < right) {
+
             int sum = arr[left] + arr[right];
-            int ch = Math.abs(sum);
-            if(ans > ch) {
-                ans = ch;
-                ansLeft = arr[left];
-                ansRight = arr[right];
-                if(ch == 0) break;
+            int curr = Math.abs(sum);
+            if(curr < ans) {
+                ans = curr;
+                al = arr[left];
+                san = arr[right];
+                if(curr == 0) break;
             }
 
-            if(sum < 0) left++;
-            else right--;
+            if(sum > 0) right--;
+            else left++;
         }
+
+        System.out.println(al + " " + san);
     }
 }
