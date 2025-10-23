84e36ec40470b0241b3d554944c2077fc22a61cf
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-10-23 16:14:46 +0900

    [Gold IV] Title: 공유기 설치, Time: 272 ms, Memory: 28952 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,53 +1,62 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static long[] arr;
     static int N, C;
+    static int[] arr;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        arr = new long[N];
-        for(int i=0;i<N;i++) {
+        arr = new int[N];
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+
         Arrays.sort(arr);
-        long left = 1;
-        long answer = 0;
-        long right = arr[N-1] - arr[0];
-        long mid = (left + right) / 2;
-
+        int left = 1, right = arr[N-1] - arr[0];
+        int ans = 0;
         while(left <= right) {
-
-            if(check(mid)) {
+            int mid = (left + right) / 2;
+            if(solve(mid)) {
+                ans = mid;
                 left = mid + 1;
-                answer = mid;
             } else {
                 right = mid - 1;
             }
-            mid = (left + right) / 2;
         }
 
-        System.out.println(answer);
+        System.out.println(ans);
     }
 
-    public static boolean check(long range) {
-        long cnt = 1, start = arr[0];
-
-        for(int i=1;i<N;i++) {
-            if(arr[i] >= start + range) {
-                start = arr[i];
+    public static boolean solve(int mid) {
+        int cnt = 0;
+        int curr = 0;
+        for(int i=0;i<N;i++) {
+            if(curr == 0) {
+                curr = arr[i];
+                cnt++;
+                continue;
+            }
+            
+            // 범위 안에 있다면 공유기 설치
+            if(arr[i] - curr >= mid) {
+                curr = arr[i];
                 cnt++;
             }
         }
+        /**
+         * 1 2 4 8 9
+         * mid 가 최대일때가 아닌 최소일때를 구해야함
+         * mid = 4,
+         */
 
-        if(cnt < C)
-            return false;
-        return true;
+        if(cnt >= C) return true;
+        else return false;
     }
 }
