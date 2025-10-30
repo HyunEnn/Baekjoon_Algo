14e1c48c95be639a642686037aaab6fa860364ee
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-10-30 10:22:33 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29068 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,7 +2,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, C;
@@ -14,7 +15,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         arr = new int[N];
-        for(int i = 0; i < N; i++) {
+        for(int i=0;i<N;i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
@@ -23,38 +24,30 @@ public class Main {
         int ans = 0;
         while(left <= right) {
             int mid = (left + right) / 2;
-            if(solve(mid)) {
-                ans = mid;
+            if(canDivide(mid)) {
                 left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+                ans = Math.max(ans, mid);
+            } else right = mid - 1;
         }
 
         System.out.println(ans);
     }
 
-    public static boolean solve(int mid) {
-        int cnt = 0;
-        int curr = 0;
+    private static boolean canDivide(int idx) {
+        // 정해진 idx 기준으로 cnt 가 C개 이상을 세울 수 있다면 true
+        int cnt = 0, curr = 0;
         for(int i=0;i<N;i++) {
             if(curr == 0) {
                 curr = arr[i];
                 cnt++;
                 continue;
             }
-            
-            // 범위 안에 있다면 공유기 설치
-            if(arr[i] - curr >= mid) {
+
+            if(arr[i] - curr >= idx) {
                 curr = arr[i];
                 cnt++;
             }
         }
-        /**
-         * 1 2 4 8 9
-         * mid 가 최대일때가 아닌 최소일때를 구해야함
-         * mid = 4,
-         */
 
         if(cnt >= C) return true;
         else return false;
