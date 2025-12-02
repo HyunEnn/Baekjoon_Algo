fc16dceb2924c3c583c7f80acd987168afa067ba
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2025-12-02 20:20:14 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 208 ms, Memory: 20816 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -6,80 +6,68 @@ import java.util.*;
 
 public class Main {
     static class Point implements Comparable<Point> {
-        int u, v, w;
-        Point(int u, int v, int w) {
-            this.u = u;
+        int v, w;
+        Point(int v, int w) {
             this.v = v;
             this.w = w;
         }
 
         @Override
         public int compareTo(Point p) {
-            if(this.w == p.w) return this.u - p.u;
             return this.w - p.w;
         }
     }
     static int N, M;
-    static List<Point> list;
+    static List<Point>[] list;
+    static boolean[] v;
     static char[] arr;
-    static int[] depth;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        v = new boolean[N + 1];
         arr = new char[N + 1];
+        list = new ArrayList[N + 1];
+        for(int i=1;i<=N;i++) {
+            list[i] = new ArrayList<>();
+        }
+
         st = new StringTokenizer(br.readLine());
         for(int i=1;i<=N;i++) {
             arr[i] = st.nextToken().charAt(0);
         }
-
-        list = new ArrayList<>();
-        depth = new int[N + 1];
-        Arrays.fill(depth, -1);
 
         for(int i=0;i<M;i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            list.add(new Point(a, b, c));
+            list[a].add(new Point(b,c));
+            list[b].add(new Point(a,c));
         }
 
-        Collections.sort(list);
+        PriorityQueue<Point> PQ = new PriorityQueue<>();
         long ans = 0;
         int cnt = 0;
+        PQ.offer(new Point(1, 0));
 
-        for(Point p : list) {
-            int start = p.u;
-            int end = p.v;
-            if((arr[start] != arr[end]) && uni(start, end)) {
-                cnt++;
-                ans += p.w;
+        while(!PQ.isEmpty()) {
+            Point p = PQ.poll();
+            if(v[p.v]) continue;
+            v[p.v] = true;
+            ans += p.w;
+            cnt++;
+            for(Point np : list[p.v]) {
+                // 이미 방문했거나, 같은 성별이라면 무시
+                if(v[np.v] || arr[p.v] == arr[np.v]) continue;
+
+                PQ.offer(np);
             }
         }
 
-        if(cnt == N-1) System.out.println(ans);
+        if(cnt == N) System.out.println(ans);
         else System.out.println(-1);
     }
-
-    private static int find(int x) {
-        if(depth[x] == -1) return x;
-        return depth[x] = find(depth[x]);
-    }
-
-    private static boolean uni(int u, int v) {
-        u = find(u);
-        v = find(v);
-
-        if(u == v) return false;
-        depth[v] = u;
-        return true;
-    }
-    /**
-     * 1. 같은 성별끼리의 루트끼리는 연결되면 안된다
-     * 2. 최소 스패닝 트리 - Prim
-     *  -
-     */
 }
