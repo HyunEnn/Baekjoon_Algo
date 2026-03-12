4c508ccedbd6bff0e97c799dfd66d5fa85566a56
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2026-03-12 14:10:02 +0900

    [level 2] Title: 호텔 대실, Time: 11.66 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,37 +1,45 @@
 import java.util.*;
 
 class Solution {
-    
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
     public int solution(String[][] book_time) {
-        int[][] arr = new int[book_time.length][2];
-        // 1. 시간을 분으로 변환 후 정렬
+        List<Point> list = new ArrayList<>();
         for(int i=0;i<book_time.length;i++) {
-            arr[i][0] = toMin(book_time[i][0]);
-            arr[i][1] = toMin(book_time[i][1]);
-        }
-        
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        
-        PriorityQueue<Integer> PQ = new PriorityQueue<>();
-        // 2. PQ에 
-        for(int i=0;i<arr.length;i++) {
-            int start = arr[i][0];
-            int end = arr[i][1] + 10;
-            if(!PQ.isEmpty() && PQ.peek() <= start) {
-                PQ.poll();
-            }
+            String[] s = book_time[i][0].split(":");
+            String[] e = book_time[i][1].split(":");
             
-            PQ.offer(end);
+            int st = Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+            int end = Integer.parseInt(e[0]) * 60 + Integer.parseInt(e[1]);
+            
+            // System.out.println(st + " " + end);
+            list.add(new Point(st, end));
         }
         
+        list.sort((a, b) -> {
+            if(a.r == b.r) return a.c - b.c;
+            return a.r - b.r;
+        });
+            
+        PriorityQueue<Integer> PQ = new PriorityQueue<>();
+        for(Point p : list) {
+            int start = p.r; int end = p.c + 10;
+            // 비었으면, 최초 삽입
+            if(PQ.isEmpty()) PQ.offer(end);
+            else {
+                int first = PQ.peek();
+                if(first <= start) {
+                    PQ.poll(); 
+                }
+                PQ.offer(end);
+            }
+        }
         int answer = PQ.size();
         return answer;
-    }
-    
-    private static int toMin(String s) {
-        String[] a = s.split(":");
-        int sum = 0;
-        sum += Integer.parseInt(a[0]) * 60 + Integer.parseInt(a[1]);
-        return sum;
     }
 }
