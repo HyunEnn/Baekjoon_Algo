43ba8fa0a5bc8e5cdf194054b8f3fa392ffdad5c
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2026-08-24 16:06:16 +0900

    [level 2] Title: 할인 행사, Time: 46.73 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,37 +1,25 @@
+
 import java.util.*;
 
 class Solution {
-    static HashMap<String, Integer> map = new HashMap<>();
+    
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int totalLen = 0;
-        for(int i=0;i<number.length;i++) {
-            totalLen += number[i];
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<want.length;i++) {
+          map.put(want[i], map.getOrDefault(want[i], 0) + number[i]);
         }
-        // 중복된 값이 올 일은 없으니, getOrDefault에 대한 고려는 필요 x
-        System.out.println(totalLen);
-        for(int i=0;i<=discount.length - totalLen;i++) {
-            // System.out.println(i + "회차");
-            for(int a=0;a<want.length;a++)
-                map.put(want[a], number[a]);
-            for(int j=i;j<i + totalLen;j++) {
-                for(int k=0;k<want.length;k++) {
-                    if(discount[j].equals(want[k])) {
-                        map.put(want[k], map.get(want[k]) - 1);
-                    }
-                }
-            }
-            // 여기서 map의 값들이 전부 0 이하면 answer에 값 추가
-            boolean flag = true;
-            for(String key : map.keySet()) {
-                // System.out.println(key + " " + map.get(key));
-                if(map.get(key) > 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) answer++;
+
+        for(int i=0;i<=discount.length-10;i++) {
+          HashMap<String, Integer> temp = new HashMap<>();
+          for(int j=i;j<i+10;j++) {
+            temp.put(discount[j], temp.getOrDefault(discount[j], 0) + 1);
+          }
+          if(map.equals(temp)) {
+            answer++;
+          }
         }
-        return answer;
+        
+      return answer;
     }
 }
