b411245093e2b006d12500bc36773562ebea7fd5
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2026-08-25 10:36:22 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.24 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,68 +1,55 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<Integer>[] list;
-    static boolean[] v;
+  static ArrayList[] list;
+    
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         list = new ArrayList[n+1];
-        for(int i=1;i<=n;i++) {
-            list[i] = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+          list[i] = new ArrayList<Integer>();
         }
-        
         for(int i=0;i<wires.length;i++) {
-            int f = wires[i][0];
-            int l = wires[i][1];
-            list[f].add(l);
-            list[l].add(f);
+          list[wires[i][0]].add(wires[i][1]);
+          list[wires[i][1]].add(wires[i][0]);
         }
-        
-        // 경우의 수를 기반으로 선을 끊었을 때, 2개의 경우로 나뉘는지
+
         for(int i=0;i<wires.length;i++) {
-            int s = wires[i][0];
-            int e = wires[i][1];
-            list[s].remove(Integer.valueOf(e));
-            list[e].remove(Integer.valueOf(s));
-            
-            v = new boolean[n+1];
-            v[1] = true;
-            int graph = recursive(1); // 1번 노드 탐색
-            int graph2 = 0;
-            // for(int j=1;j<=n;j++) {
-            //     System.out.print(v[j] + " ");
-            // }
-            // System.out.println();
-            // 탐색 후에 v가 하나라도 false있으면 그 노드를 기반으로 탐색
-            for(int j=1;j<=n;j++) {
-                if(!v[j]) {
-                    v[j] = true;
-                    graph2 = recursive(j);
-                }
-            }
-            // System.out.println(graph + " " + graph2);
-            answer = Math.min(answer, Math.abs(graph - graph2));
-            // 끝난 후에 되돌리기
-            list[s].add(e);
-            list[e].add(s);
+          int a = wires[i][0];
+          int b = wires[i][1];
+          // 현재 값을 remove 하고, 다시 복원하면서 가능한 전선의 갯수가 최대한 비슷한 경우를 탐색
+          list[a].remove(Integer.valueOf(b));
+          list[b].remove(Integer.valueOf(a));
+
+          int cnt = bfs();
+          int diff = Math.abs(n - cnt - cnt);
+          answer = Math.min(answer, diff);
+          // 복원
+          list[a].add(b);
+          list[b].add(a);
         }
         return answer;
     }
+
+    private static int bfs() {
+      Queue<Integer> Q = new ArrayDeque<>();
+      Q.offer(1);
     
-    public static int recursive(int idx) {
-        Queue<Integer> Q = new ArrayDeque<>();
-        Q.offer(idx);
-        int cnt = 0;
-        
-        while(!Q.isEmpty()) {
-            int p = Q.poll();
-            cnt++;
-            for(int i : list[p]) {
-                if(!v[i]) {
-                    v[i] = true;
-                    Q.offer(i);
-                }
-            }
+      int count = 1;
+      boolean[] visited = new boolean[list.length + 1];
+        visited[1] = true;
+      // Implementation for BFS
+      while(!Q.isEmpty()) {
+        int p = Q.poll();
+        for(int i=0;i<list[p].size();i++) {
+          int next = (int)list[p].get(i);
+          if(!visited[next]) {
+            visited[next] = true;
+            count++;
+            Q.offer(next);
+          }
         }
-        return cnt;
+      }
+      return count;
     }
 }
